a62e4589a19d2cd59a63fd42ec3d267eee1c02e0
“LipiZed” <kometa1810@gmail.com>
2025-02-24 22:45:40 +0300

    Miltiples of 3 or 5 Kyu 6

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("second");
+﻿using CodeWars.Tasks.CodeWars;
+
+Multiples_of_3_or_5 x = new Multiples_of_3_or_5();
+
+int v = x.Solution(200);
+
+Console.WriteLine(v);
---
+++ CodeWars/Tasks/CodeWars/Multiples of 3 or 5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tasks.CodeWars
+{
+    public class Multiples_of_3_or_5
+    {
+        public int Solution(int num)
+        {
+            if (num <= 3)
+            {
+                return 0;
+            }
+            List<int> nums = new List<int>();
+            for (int i = 0; 3 * i < num; i++)
+            {
+                nums.Add(3 * i);
+            }
+            for (int i = 0; 5 * i < num; i++)
+            {
+                nums.Add(5 * i);
+            }
+            return nums.Distinct().Sum();
+        }
+    }
+}
