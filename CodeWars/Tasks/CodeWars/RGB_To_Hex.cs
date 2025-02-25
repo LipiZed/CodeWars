6dfe467384c89dff4d6e27d5641349c2f87deca8
“LipiZed” <kometa1810@gmail.com>
2025-02-25 21:23:25 +0300

    Start of new

---
+++ CodeWars/Tasks/CodeWars/RGB_To_Hex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tasks.CodeWars
+{
+    public class RGB_To_Hex
+    {
+        public string Rgb(int r, int g, int b)
+        {
+            if (r > 255) r = 255;
+            if (g > 255) g = 255;
+            if (b > 255) b = 255;
+
+            return Convert.ToString(r, 16).ToUpper() + Convert.ToString(g, 16).ToUpper() + Convert.ToString(b, 16).ToUpper();
+        }
+    }
+}
