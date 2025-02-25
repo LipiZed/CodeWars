5368faf5e26084b6c53a5ea35a6039055dfd0807
LipiZed <kometa1810@gmail.com>
2025-02-26 00:00:02 +0300

    Snail Sort start

---
+++ CodeWars/Tasks/CodeWars/SnailSolution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tasks.CodeWars
+{
+    public class SnailSolution
+    {
+        public int[] Snail(int[][] array)
+        {
+            array = [[1,2,3],
+                     [4,5,6],
+                     [7,8,9]];
+            // 0.0 0.1 0.2 1.2 2.2 2.1 2.0 1.0 1.1
+            return null;
+        }
+    }
+}
