62ea2777f1ac6a619b5bc54c45d4ad5a31ddc18a
“LipiZed” <kometa1810@gmail.com>
2025-03-05 20:10:51 +0300

    Сделано, но нужно ускорить

--- CodeWars/Tasks/LeetCode/Median.cs
+++ CodeWars/Tasks/LeetCode/Median.cs
@@ -4,6 +4,15 @@ public class Median
 {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) 
     {
-        return (double)(nums1.Sum() + nums2.Sum()) / (nums1.Count() + nums2.Count());
+        int[] result_array = nums1.Concat(nums2).ToArray();
+        Array.Sort(result_array);
+        if (result_array.Length % 2 == 0)
+        {
+            return (double)(result_array[result_array.Length / 2] + result_array[result_array.Length / 2 - 1]) / 2;
+        }
+        else
+        {
+            return (double)result_array[result_array.Length / 2];
+        }
     }
 }
