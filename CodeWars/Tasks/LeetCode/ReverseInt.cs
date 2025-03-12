ca6150371f73fb9bdccef6c67bef7cc55805c50c
LipiZed <kometa1810@gmail.com>
2025-03-12 22:42:42 +0300

    Сделал, но не так как все, хотя по скорости 70% обошел

---
+++ CodeWars/Tasks/LeetCode/ReverseInt.cs
@@ -0,0 +1,31 @@
+﻿namespace CodeWars.Tasks.LeetCode;
+using System.Linq;
+public class ReverseInt
+{
+    public int reverse(int x)
+    {
+        if (x < 0)
+        {
+            string result = new string(x.ToString().Remove(0,1).Reverse().ToArray());
+            try
+            {
+                int result_int = int.Parse("-" + result);
+                return result_int;
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+        string result2 = new string(x.ToString().Reverse().ToArray());
+        try
+        {
+            int result_int2 = int.Parse(result2);
+            return result_int2;
+        }
+        catch (Exception e)
+        {
+            return 0;
+        }
+    }
+}
