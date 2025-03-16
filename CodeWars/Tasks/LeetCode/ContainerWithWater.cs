b4320f64c3f8d8d2a7a93b314129bb083aa793fa
LipiZed <kometa1810@gmail.com>
2025-03-16 18:20:09 +0300

    Beats 99.92%

---
+++ CodeWars/Tasks/LeetCode/ContainerWithWater.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeWars.Tasks.LeetCode;
+
+public class ContainerWithWater
+{
+    public int MaxArea(int[] height)
+    {
+        int area = 0;
+        for (int i = 0, j = height.Length - 1; i < j;)
+        {
+            area = Math.Max(area, Math.Min(height[i], height[j]) * (j - i));
+            if (height[i] < height[j])
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        return area;
+    }
+}
