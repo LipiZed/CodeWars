b553bb647f1f9a4d7b4cf28900de390b63afcca3
LipiZed <kometa1810@gmail.com>
2025-03-16 17:52:04 +0300

    Средняя скорость по результатам

---
+++ CodeWars/Tasks/LeetCode/StringToInteger.cs
@@ -0,0 +1,45 @@
+﻿namespace CodeWars.Tasks.LeetCode;
+
+public class StringToInteger
+{
+    public int MyAtoi(string s)
+    {
+        s = s.Trim(' ');
+        char[] specialChars = ['.', ',', '/', ' '];
+        string result_string = "";
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!char.IsLetter(s[i]) || specialChars.Contains(s[i]))
+            {
+                if ((s[i] == '-' || s[i] == '+') && i != 0  || specialChars.Contains(s[i]))
+                {
+                    break;
+                }
+
+                result_string += s[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        if (string.IsNullOrEmpty(result_string))
+        {
+            return 0;
+        }
+
+        try
+        {
+            return int.Parse(result_string);
+        }
+        catch (OverflowException)
+        {
+            return result_string[0] == '-' ? int.MinValue : int.MaxValue;
+        }
+        catch (FormatException)
+        {
+            return 0;
+        }
+    }
+}
