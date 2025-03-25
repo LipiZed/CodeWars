0b4fccb6c3b9434f89a9e4527833851e6dc502d4
LipiZed <kometa1810@gmail.com>
2025-03-25 16:07:13 +0300

    В целом неплохо

---
+++ CodeWars/Tasks/LeetCode/Arrays & Hashing/ContainsDuplicate.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeWars.Tasks.LeetCode.Arrays___Hashing;
+
+public class ContainsDuplicate
+{
+    public bool Contains(int[] nums) 
+    {
+        if (nums.Length != nums.Distinct().Count())
+        {
+            return true;
+        }
+        return false;
+    }
+}
